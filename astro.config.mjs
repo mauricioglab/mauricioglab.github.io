626477495926dee4649a1bbdea8be2125c9de961
mauriciogtask <tu-email@mauricioglab.com>
2026-01-25 18:22:22 -0300

    instalado tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   site: "https://YOUR_GITHUB_USERNAME.github.io",
   base: "/mglab-spa-depot",
   trailingSlash: "ignore",
   server: { open: "/mglab-spa-depot/" },
+  integrations: [tailwind()],
 });
